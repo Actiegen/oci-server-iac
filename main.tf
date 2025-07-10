814988254060083182bd002897e15e2b6118ce6f
Actiegen <actiegen@gmail.com>
2025-07-10 02:13:24 -0300

    Add provider config

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "oci" {
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  private_key = var.private_key
+  region = var.region
+}
 module "compute_instance" {
   source              = "./modules/compute-instance"
   availability_domain = var.availability_domain
--- variables.tf
+++ variables.tf
@@ -9,5 +9,6 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
+variable "private_key" {}
 variable "region" {}
 variable "auth" {}
