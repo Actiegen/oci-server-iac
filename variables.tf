424dd60342fc99daa76613d48d6175a4490b562a
Actiegen <actiegen@gmail.com>
2025-07-10 03:41:02 -0300

    Removing unused variable

--- variables.tf
+++ variables.tf
@@ -8,7 +8,6 @@ variable "namespace" {}
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key_path" {}
 variable "private_key" {}
 variable "region" {}
 variable "auth" {}
